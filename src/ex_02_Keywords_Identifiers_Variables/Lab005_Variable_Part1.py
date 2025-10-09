94d1d63f4acdf35a919e07df6bc4d1814383d25c
Priya-Rajput-QA <priya.rajput23394@gmail.com>
2025-10-09 19:09:38 +0530

    9-Oct-2025 Code Commit and Push

---
+++ src/ex_02_Keywords_Identifiers_Variables/Lab005_Variable_Part1.py
@@ -0,0 +1,12 @@
+age = 65
+age  = "Priya"
+print(age)
+# Dynamic Typed Language. - It can understand you are doing, want.
+pi = 3.14
+name = "Priya"
+name = "Priya"
+name = "Priya"
+name = "Priya"
+name = "Priya"
+name = "Rajput"
+print(name)
