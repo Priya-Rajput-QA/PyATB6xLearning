401861797a7b90ed202d899a39c6cb8d956c3f7e
Priya-Rajput-QA <priya.rajput23394@gmail.com>
2025-10-13 15:44:25 +0530

    13-Oct-2025 Code Commit and Push

---
+++ src/ex_02_Keywords_Identifiers_Variables/Lab007_Variables_Names.py
@@ -0,0 +1,17 @@
+#123abc = 90
+_priya = "shivam"
+_abc = 23
+pi = 3.14
+name = "Priya"
+isMale = True
+
+# Type
+print(type(_priya)) # <class 'str'>
+print(type(pi)) # <class 'float'>
+print(type(isMale)) #<class 'bool'>
+
+complex_number = 2+3j
+print(complex_number)
+print(complex_number.real)
+print(complex_number.imag)
+print(type(complex_number))
